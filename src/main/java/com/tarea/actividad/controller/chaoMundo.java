7d07a9e7f50f1c6f3267f53c1742030eb9d2ff51
FabianReyes02 <fa.reyeso@duocuc.cl>
2026-08-12 10:58:35 -0400

    V1.1.0 nuevo controller

---
+++ src/main/java/com/tarea/actividad/controller/chaoMundo.java
@@ -0,0 +1,14 @@
+package com.tarea.actividad.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class chaoMundo {
+    @GetMapping("/despedida")
+    public static String chao() {
+        return "Chao Mundo";
+    }
+}
