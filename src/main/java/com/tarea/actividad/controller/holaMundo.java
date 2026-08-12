677ca96eb7058973caab0ca3b4ec7196eb8d8412
FabianReyes02 <fa.reyeso@duocuc.cl>
2026-08-12 11:03:23 -0400

    V1.1.1 actualizacion mensaje

--- src/main/java/com/tarea/actividad/controller/holaMundo.java
+++ src/main/java/com/tarea/actividad/controller/holaMundo.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1")
 public class holaMundo {
-    @GetMapping("/")
+    @GetMapping("/saludo")
     public static String saludos() {
         return "Hola Mundo";
     }
